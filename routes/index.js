739f9dc3b575e2475b28b7bc1678c514d05cf9fc
magic3dcn <magic3d@163.com>
2014-12-10 15:16:21 +0800

    Update homepage resource.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+router.get('/doreg', function(req, res) {
+  res.render('DoReg');
+});
+
+router.get('/dologin', function(req, res) {
+  res.render('DoLogin');
+});
+
+router.get('/aboutus', function(req, res) {
+  res.render('AboutUs');
+});
+
+router.get('/innerspacedesign', function(req, res) {
+  res.render('InnerSpaceDesign');
+});
+
+router.get('/furnituredesign', function(req, res) {
+  res.render('FurnitureDesign');
+});
+
+router.get('/designshow', function(req, res) {
+  res.render('DesignShow');
+});
+
 module.exports = router;
