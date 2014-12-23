e4c4f6098e01a8a85e8199e2d5c5162651b05a74
magic3dcn <magic3d@163.com>
2014-12-23 16:43:58 +0800

    Add mouse callback function.

---
+++ public/javascripts/LayoutDesignInteraction.js
@@ -0,0 +1,9 @@
+﻿function mousedown()
+{
+	console.log("mousedown:", event.clientX, event.clientY, event.offsetX, event.offsetY);
+} 
+
+function mouseup()
+{
+	console.log("mouseup:", event.clientX, event.clientY);
+}
