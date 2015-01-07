e70f733b04f6102f0559865d6993facab154c48e
magic3dcn <magic3d@163.com>
2015-01-07 12:50:53 +0800

    Update.

--- public/javascripts/LayoutDesignFramework.js
+++ public/javascripts/LayoutDesignFramework.js
@@ -24,9 +24,28 @@ REAL3D.AppManager =
 {
 	Init : function()
 	{
+		var initApp = new REAL3D.HelloApp();
+		this.EnterApp(initApp);
 	},
 
 	Update : function()
+	{
+		this.currentApp.Update();
+	},
+
+	EnterApp : function(app)
+	{
+		
+		this.appSet.push(app);
+		this.currentApp = app;
+	},
+
+	SwitchCurrentApp : function(appName)
+	{
+
+	},
+
+	GetApp : function(appName)
 	{
 
 	},
@@ -34,17 +53,22 @@ REAL3D.AppManager =
 	MouseDown : function(e)
 	{
 		console.log("AppManager: mousedown:", e.clientX, e.clientY, e.offsetX, e.offsetY);
+		this.currentApp.MouseDown(e);
 	},
 
 	MouseUp :  function(e)
 	{
 		console.log("AppManager: mouseup:", e.clientX, e.clientY);
+		this.currentApp.MouseUp(e);
 	},
 
 	MouseMove : function(e)
 	{
+		this.currentApp.MouseMove(e);
+	},
 
-	}
+	currentApp : null,
+	appSet : []
 }
 
 REAL3D.Listener = 
@@ -91,42 +115,65 @@ REAL3D.RenderManager.Init = function()
 	this.scene = new THREE.Scene();
 	var windowWidth = 1024;
 	var windowHeight = 768;
-	this.camera1 = new THREE.PerspectiveCamera( 75, windowWidth / windowHeight, 0.1, 1000 );
-	this.camera1.position.set(0, 0, 10);
-	this.camera2 = new THREE.OrthographicCamera( -5, 5, -5, 5, 1, 1000);
-	this.camera2.position.set(0, 0, 10);
+	this.camera = new THREE.PerspectiveCamera( 75, windowWidth / windowHeight, 0.1, 1000 );
+	this.camera.position.set(0, 0, 10);
 	this.renderer = new THREE.WebGLRenderer();
 	this.renderer.setSize( windowWidth, windowHeight);
 	var geometry = new THREE.BoxGeometry( 2, 2, 2 );
 	var material = new THREE.MeshBasicMaterial( { color: 0xfefefe } );
 	this.cube = new THREE.Mesh( geometry, material );
+	var geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
+	var material2 = new THREE.MeshBasicMaterial({color: 0x9efe9e});
+	this.cube2 = new THREE.Mesh(geometry2, material2);
+	this.cube2.position.x = 5;
+	this.cube.add(this.cube2);
 	this.scene.add( this.cube );
+	//this.scene.add( this.cube2);
 	return this.renderer.domElement;
 }
 
 REAL3D.RenderManager.Update = function()
 {
-	this.cube.rotation.x += 0.005;
-    this.cube.rotation.y += 0.005;
+	this.cube.rotation.x += 0.001;
+    this.cube.rotation.y += 0.05;
 
-    if (this.cameraType < 250) 
-    {
-    	this.renderer.render(this.scene, this.camera1);
-    	this.cameraType++;
-    }
-    else
-    {
-    	this.renderer.render(this.scene, this.camera2);
-    	this.cameraType++;
-    	if (this.cameraType == 500) 
-    	{
-    		this.cameraType = 1;
-    	};
-    }
+    this.renderer.render(this.scene, this.camera);
 }
 
 
 REAL3D.AppBase = function()
-{
+{}
 
+REAL3D.AppBase.prototype = 
+{
+	Enter : function()
+	{},
+
+	Update : function()
+	{},
+
+	Exit : function()
+	{},
+
+	MouseDown : function(e)
+	{},
+
+	MouseUp : function(e)
+	{},
+
+	MouseMove : function(e)
+	{}
+}
+
+REAL3D.HelloApp = function()
+{
+	REAL3D.AppBase.call(this);
+	this.appName = "HelloApp";
+}
+
+REAL3D.HelloApp.prototype = Object.create(REAL3D.AppBase.prototype);
+
+REAL3D.HelloApp.prototype.MouseDown = function(e)
+{
+	console.log("HelloApp MouseDown: ", e.clientX, e.clientY, e.offsetX, e.offsetY);
 }
