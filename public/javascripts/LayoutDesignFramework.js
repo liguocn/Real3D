ea915d203cfca92192ed02eff8e07a57261437d4
magic3dcn <magic3d@163.com>
2015-01-08 10:20:49 +0800

    Add global ratation to THREE.Object3D.

--- public/javascripts/LayoutDesignFramework.js
+++ public/javascripts/LayoutDesignFramework.js
@@ -56,10 +56,16 @@ REAL3D.AppManager =
 		}
 		if (this.currentApp != null)
 		{
+			if (this.currentApp.appName == appName)
+			{
+				console.log("Switch to the same app: ", appName);
+				return true;
+			}
 			this.currentApp.Exit();
 		}
 		this.currentApp = this.appSet[appName];
 		this.currentApp.Enter();
+		return true;
 	},
 
 	GetApp : function(appName)
@@ -91,8 +97,9 @@ REAL3D.Listener =
 	Init : function(dom)
 	{
 		var that = this;
-		dom.addEventListener('mousedown', function(e) { that.MouseDown(e); }, false );
-		dom.addEventListener('mouseup', function(e) { that.MouseUp(e); }, false );
+		dom.addEventListener("mousedown", function(e) { that.MouseDown(e); }, false );
+		dom.addEventListener("mouseup", function(e) { that.MouseUp(e); }, true );
+		dom.addEventListener("mousemove", function(e) {that.MouseMove(e); }, true);
 	},
 
 	MouseDown : function(e)
@@ -133,10 +140,13 @@ REAL3D.RenderManager.Init = function()
 	var geometry = new THREE.BoxGeometry( 2, 2, 2 );
 	var material = new THREE.MeshBasicMaterial( { color: 0xfefefe } );
 	this.cube = new THREE.Mesh( geometry, material );
+	this.cube.name = "root";
+	this.cube.position.set(1, 1, 1);
 	var geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
 	var material2 = new THREE.MeshBasicMaterial({color: 0x9efe9e});
 	this.cube2 = new THREE.Mesh(geometry2, material2);
 	this.cube2.position.x = 5;
+	this.cube2.name = "cube2";
 	this.cube.add(this.cube2);
 	this.scene.add( this.cube );
 	return this.renderer.domElement;
@@ -144,8 +154,8 @@ REAL3D.RenderManager.Init = function()
 
 REAL3D.RenderManager.Update = function()
 {
-	this.cube.rotation.x += 0.001;
-    this.cube.rotation.y += 0.05;
+	//this.cube.rotation.x += 0.001;
+    //this.cube.rotation.y += 0.05;
 
     this.renderer.render(this.scene, this.camera);
 }
@@ -196,4 +206,73 @@ REAL3D.HelloApp.prototype.Exit = function()
 REAL3D.HelloApp.prototype.MouseDown = function(e)
 {
 	console.log("HelloApp MouseDown: ", e.clientX, e.clientY, e.offsetX, e.offsetY);
+}
+
+REAL3D.LayoutDesignApp = function()
+{
+	REAL3D.AppBase.call(this);
+	this.appName = "LayoutDesignApp";
+	this.mousePos = new THREE.Vector2(0, 0);
+}
+
+REAL3D.LayoutDesignApp.prototype = Object.create(REAL3D.AppBase.prototype);
+
+REAL3D.LayoutDesignApp.prototype.Enter = function()
+{
+	console.log("Enter LayoutDesignApp");
+}
+
+REAL3D.LayoutDesignApp.prototype.Exit = function()
+{
+	console.log("Exit LayoutDesignApp");
+}
+
+REAL3D.LayoutDesignApp.prototype.MouseDown = function(e)
+{
+	console.log("LayoutDesignApp MouseDown: ", e.clientX, e.clientY, e.offsetX, e.offsetY);
+	this.mousePos.set(e.offsetX, e.offsetY);
+}
+
+REAL3D.LayoutDesignApp.prototype.MouseMove = function(e)
+{
+	//console.log("Mouse move: ", e.which);
+	if (e.which == 1)
+	{
+		console.log("Left mouse move");
+		var mouseDifX = e.offsetX - this.mousePos.x;
+		var mouseDifY = e.offsetY - this.mousePos.y;
+		var rotateObj = REAL3D.RenderManager.scene.getObjectByName("root", true);
+		rotateObj.rotateXGlobal(mouseDifY / 500);
+		rotateObj.rotateYGlobal(mouseDifX / 500);
+		this.mousePos.set(e.offsetX, e.offsetY);
+	}
+	else if (e.which == 2)
+	{
+		console.log("Middle mouse move");
+	}
+	else if (e.which == 3)
+	{
+		console.log("Right mouse move");
+	}
+}
+
+REAL3D.LayoutDesignApp.prototype.MouseUp = function(e)
+{
+
+}
+
+function EnterDesignSpaceApp()
+{
+	var layoutDesignApp = new REAL3D.LayoutDesignApp();
+	REAL3D.AppManager.EnterApp(layoutDesignApp);
+}
+
+function SwitchToHelloApp()
+{
+	REAL3D.AppManager.SwitchCurrentApp("HelloApp");
+}
+
+function SwitchToDesignSpaceApp()
+{
+	REAL3D.AppManager.SwitchCurrentApp("LayoutDesignApp");
 }
