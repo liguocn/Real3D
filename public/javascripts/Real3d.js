a909381942c1856ffface9401a51449b82a8fa9d
magic3dcn <magic3d@163.com>
2015-01-09 10:52:13 +0800

    Add Publisher.

--- public/javascripts/Real3d.js
+++ public/javascripts/Real3d.js
@@ -215,3 +215,74 @@ REAL3D.HelloApp.prototype.MouseDown = function(e)
 {
 	console.log("HelloApp MouseDown: ", e.clientX, e.clientY, e.offsetX, e.offsetY);
 }
+
+REAL3D.Publisher = function() {
+    this.messageTypes = {};
+}
+
+REAL3D.Publisher.prototype.subscribe = function(message, subscriber, callback) {
+    var subscribers = this.messageTypes[message];
+    if (subscribers)
+    {
+        if (this.findSubscriber(subscribers, subscriber) != -1)
+        {
+            return;
+        }
+    }
+    else
+    {
+        subscribers = [];
+        this.messageTypes[message] = subscribers;
+    }
+
+    subscribers.push({ subscriber : subscriber, callback : callback });
+}
+
+REAL3D.Publisher.prototype.unsubscribe = function(message, subscriber, callback) {
+    if (subscriber)
+    {
+        var subscribers = this.messageTypes[message];
+
+        if (subscribers)
+        {
+            var i = this.findSubscriber(subscribers, subscriber, callback);
+            if (i != -1)
+            {
+                this.messageTypes[message].splice(i, 1);
+            }
+        }
+    }
+    else
+    {
+        delete this.messageTypes[message];
+    }
+}
+
+REAL3D.Publisher.prototype.publish = function(message) {
+    var subscribers = this.messageTypes[message];
+
+    if (subscribers)
+    {
+        for (var i = 0; i < subscribers.length; i++)
+        {
+            var args = [];
+            for (var j = 0; j < arguments.length - 1; j++)
+            {
+                args.push(arguments[j + 1]);
+            }
+            subscribers[i].callback.apply(subscribers[i].subscriber, args);
+        }
+    }
+}
+
+REAL3D.Publisher.prototype.findSubscriber = function (subscribers, subscriber) {
+    for (var i = 0; i < subscribers.length; i++)
+    {
+        if (subscribers[i] == subscriber)
+        {
+            return i;
+        }
+    }
+    
+    return -1;
+}
