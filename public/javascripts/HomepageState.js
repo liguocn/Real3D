0faec915e4776374c3a7300dcb77a6a5c7de3ceb
magic3dcn <magic3d@163.com>
2015-01-20 12:31:18 +0800

    Add HomepageState file.

---
+++ public/javascripts/HomepageState.js
@@ -0,0 +1,20 @@
+/*global REAL3D */
+REAL3D.HomepageState = function() {
+    "use strict";
+    REAL3D.StateBase.call(this);
+    this.stateName = "HomepageState";
+};
+
+REAL3D.HomepageState.prototype = Object.create(REAL3D.StateBase.prototype);
+
+REAL3D.HomepageState.prototype.enter = function() {
+    "use strict";
+};
+
+REAL3D.HomepageState.prototype.update = function() {
+    "use strict";
+};
+
+REAL3D.HomepageState.prototype.exit = function() {
+    "use strict";
+};
