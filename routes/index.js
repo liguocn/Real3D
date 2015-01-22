3eed5bb879b9634162798c6c23d990a7485ee081
magic3dcn <magic3d@163.com>
2015-01-22 09:40:55 +0800

    Update register and login.

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,46 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index');
+    res.render('index');
 });
 
-router.get('/doreg', function(req, res) {
-  res.render('DoReg');
+router.get("/doreg", function(req, res) {
+    res.render("DoReg");
+})
+
+router.post('/doreg', function(req, res) {
+    console.log("    doreg--username: ", req.body["username"], "session: ", req.session.user);
+    req.session.user = req.body["username"];
+    res.redirect("/innerspacedesign");
 });
 
 router.get('/dologin', function(req, res) {
-  res.render('DoLogin');
+    res.render('DoLogin');
 });
 
+router.post("/doLogin", function(req, res) {
+    console.log("    doLogin--username: ", req.body["username"], "session: ", req.session.user);
+    if (req.session.user === req.body["username"]) {
+        res.redirect("/innerspacedesign");
+    } else {
+        res.redirect("/doreg");
+    }
+})
+
 router.get('/aboutus', function(req, res) {
-  res.render('AboutUs');
+    res.render('AboutUs');
 });
 
 router.get('/innerspacedesign', function(req, res) {
-  res.render('InnerSpaceDesign');
+    res.render('InnerSpaceDesign');
 });
 
 router.get('/furnituredesign', function(req, res) {
-  res.render('FurnitureDesign');
+    res.render('FurnitureDesign');
 });
 
 router.get('/designshow', function(req, res) {
-  res.render('DesignShow');
+    res.render('DesignShow');
 });
 
 module.exports = router;
