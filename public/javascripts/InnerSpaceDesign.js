f8db1f36e602b2538f1636f7c7bc7291a3d18149
magic3dcn <magic3d@163.com>
2015-03-02 15:46:18 +0800

    update.

--- public/javascripts/InnerSpaceDesign.js
+++ public/javascripts/InnerSpaceDesign.js
@@ -1,6 +1,10 @@
 /*jslint plusplus: true */
 /*global window */
-function editDesignItem(designName) {
+
+REAL3D.InnerSpaceDesign = {
+};
+
+REAL3D.InnerSpaceDesign.editDesignItem = function (designName) {
     "use strict";
     console.log("editDesignItem: ", designName);
     var subForm, item1, item2;
@@ -16,12 +20,13 @@ function editDesignItem(designName) {
     console.log("subForm: ", subForm);
     console.log("designName: ", designName);
     subForm.submit();
-}
+};
 
-function getDesignItemsFromServer() {
+REAL3D.InnerSpaceDesign.getDesignItemsFromServer = function () {
     "use strict";
     console.log("getDesignItemsFromServer...");
-    var designNames, itemLen, item, itemId;
+    var designNames, itemLen, item, itemId, that;
+    that = this;
     $.get("/innerspacedesign/getitems", function (data) {
         if (data.success) {
             console.log("data: ", data);
@@ -36,7 +41,7 @@ function getDesignItemsFromServer() {
                 item.click(function () {
                     //var designName = $(this).prop('id');
                     var designName = $(this).text();
-                    editDesignItem(designName);
+                    that.editDesignItem(designName);
                 });
                 item.appendTo('#designSet');
             }
@@ -45,19 +50,19 @@ function getDesignItemsFromServer() {
             console.log("getDesignItemsFromServer is failed");
         }
     });
-}
+};
 
-function backToPersonalHomePage() {
+REAL3D.InnerSpaceDesign.backToPersonalHomePage = function () {
     "use strict";
     window.location.href = "/personalhomepage";
-}
+};
 
 $(document).ready(function () {
     console.log("document.ready...");
-    $('<div id="plink" class="menuitem" onclick="backToPersonalHomePage()">个人主页</div>').appendTo('#menu');
+    $('<div id="plink" class="menuitem" onclick="REAL3D.InnerSpaceDesign.backToPersonalHomePage()">个人主页</div>').appendTo('#menu');
     $('<div id="new" class="designitem">新建</div>').appendTo('#designSet');
     $('#new').click(function () {
-        editDesignItem('');
+        REAL3D.InnerSpaceDesign.editDesignItem('');
     });
     // var item = $('<div class="designitem"></div>');
     // item.text("new");
@@ -68,5 +73,5 @@ $(document).ready(function () {
     // });
     //item.appendTo('#designSet');
     console.log("value: ", $("#plink").prop('id'));
-    getDesignItemsFromServer();
+    REAL3D.InnerSpaceDesign.getDesignItemsFromServer();
 });
