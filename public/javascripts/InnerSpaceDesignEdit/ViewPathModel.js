326673f1f2f51239e25432cc460922b93f76739f
magic3dcn <magic3d@163.com>
2015-03-05 16:50:15 +0800

    update.

--- public/javascripts/InnerSpaceDesignEdit/ViewPathModel.js
+++ public/javascripts/InnerSpaceDesignEdit/ViewPathModel.js
@@ -4,3 +4,16 @@
 REAL3D.ViewPath = {
 };
 
+REAL3D.ViewPath.PathPoint = function (userPoint) {
+    "use strict";
+    this.userPoint = userPoint;
+    this.edges = [];
+};
+
+REAL3D.ViewPath.PathPoint.prototype = Object.create(REAL3D.Publisher.prototype);
+
+REAL3D.ViewPath.PathEdge = function (pathPoint0, pathPoint1) {
+    "use strict";
+    this.pathPoints = [pathPoint0, pathPoint1];
+    this.edgeLength = REAL3D.Vector2.sub(pathPoint0.userPoint.pos, pathPoint1.userPoint.pos).length();
+};
