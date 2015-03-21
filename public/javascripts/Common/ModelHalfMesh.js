8e1ccd94ed66130da48b871fb13e777e8857dc3b
liguo <liguo.magic3d@gmail.com>
2015-03-21 20:53:56 +0800

    Add Half Mesh File

---
+++ public/javascripts/Common/ModelHalfMesh.js
@@ -0,0 +1,21 @@
+/*jslint plusplus: true, continue: true */
+/*global REAL3D, THREE, console */
+
+REAL3D.MeshModel = {
+};
+
+REAL3D.MeshModel.HVertex = function () {
+    "use strict";
+};
+
+REAL3D.MeshModel.HEdge = function () {
+    "use strict";
+};
+
+REAL3D.MeshModel.HFace = function () {
+    "use strict";
+};
+
+REAL3D.MeshModel.HMesh = function () {
+    "use strict";
+};
