151c29564fef8ff34e4d49c6aed5db46ae4ff1b2
magic3dcn <magic3d@163.com>
2015-03-25 16:14:31 +0800

    update.

--- public/javascripts/CageModeling/UIEditCage.js
+++ public/javascripts/CageModeling/UIEditCage.js
@@ -22,6 +22,24 @@ REAL3D.CageModeling.EditCageUI.enter = function () {
     $('<button id="normalizeViewBut" class="topButton">N</button>').appendTo('#toolBar');
     $('#normalizeViewBut').click(function () { that.normalizeView(); });
     $('<hr />').appendTo('#toolBar');
+
+    $('<div id="editMode">编辑模式</div>').appendTo('#toolBar');
+    $('<div><input type="radio" id="editParm" name="editRadio">参数编辑</div>').appendTo('#editMode');
+    $('#editParm').get(0).addEventListener("click", function () { that.switchEditModeToEditParm(); });
+    $('<div><input type="radio" id="extrude" name="editRadio">挤出</div>').appendTo('#editMode');
+    $('#extrude').get(0).addEventListener("click", function () { that.switchEditModeToExtrude(); });
+    $('<div><input type="radio" id="transform" name="editRadio">移动</div>').appendTo('#editMode');
+    $('#transform').get(0).addEventListener("click", function () { that.switchEditModeToTransform(); });
+    $('<div><input type="radio" id="split" name="editRadio">分割</div>').appendTo('#editMode');
+    $('#split').get(0).addEventListener("click", function () { that.switchEditModeToSplit(); });
+    $('<div><input type="radio" id="remove" name="editRadio">删除</div>').appendTo('#editMode');
+    $('#remove').get(0).addEventListener("click", function () { that.switchEditModeToRemove(); });
+    $('<div><input type="radio" id="merge" name="editRadio">合并</div>').appendTo('#editMode');
+    $('#merge').get(0).addEventListener("click", function () { that.switchEditModeToMerge(); });
+    $('<div><input type="radio" id="connect" name="editRadio">连接</div>').appendTo('#editMode');
+    $('#connect').get(0).addEventListener("click", function () { that.switchEditModeToConnect(); });
+    $('<hr />').appendTo('#toolBar');
+    $('#editParm').get(0).checked = true;
 };
 
 REAL3D.CageModeling.EditCageUI.exit = function () {
@@ -54,4 +72,32 @@ REAL3D.CageModeling.EditCageUI.enterCreateCageUI = function () {
     this.exit();
     REAL3D.CageModeling.enterState(REAL3D.CageModeling.CreateCageState);
     REAL3D.CageModeling.CreateCageUI.enter();
+};
+
+REAL3D.CageModeling.EditCageUI.switchEditModeToEditParm = function () {
+    "use strict";
+};
+
+REAL3D.CageModeling.EditCageUI.switchEditModeToExtrude = function () {
+    "use strict";
+};
+
+REAL3D.CageModeling.EditCageUI.switchEditModeToTransform = function () {
+    "use strict";
+};
+
+REAL3D.CageModeling.EditCageUI.switchEditModeToSplit = function () {
+    "use strict";
+};
+
+REAL3D.CageModeling.EditCageUI.switchEditModeToRemove = function () {
+    "use strict";
+};
+
+REAL3D.CageModeling.EditCageUI.switchEditModeToMerge = function () {
+    "use strict";
+};
+
+REAL3D.CageModeling.EditCageUI.switchEditModeToConnect = function () {
+    "use strict";
 };
